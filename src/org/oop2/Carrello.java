6b47d286f1816cb9f3c71b97d2c63d7614307067
SebastianBuftea <sebibuftea85@gmail.com>
2024-05-20 16:51:24 +0200

    first commit

--- src/org/oop2/Carrello.java
+++ src/org/oop2/Carrello.java
@@ -1,2 +1,34 @@
-package org.oop2;public class Carrello {
+package org.oop2;
+import java.util.Scanner;
+
+public class Carrello {
+    public static void main(String[] args) {
+
+        String name;
+        String description;
+        String price;
+        String iva;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Inserire il nome: ");
+        name = in.nextLine();
+
+        System.out.print("Inserire la descrizione ");
+        description = in.nextLine();
+
+        System.out.print("Inserire il prezzo: ");
+        price = in.nextLine();
+
+        System.out.print("Inserire l iva: ");
+        iva = in.nextLine();
+
+
+
+        Prodotto prodotto1= new Prodotto( name , description , price, iva);
+        System.out.println("codice prodotto: "+prodotto1.getCode());
+        System.out.println("nome completo: "+prodotto1.getNameComplete());
+        System.out.println("prezzo con iva: "+prodotto1.getPriceWithIva());
+        System.out.println("prezzo senza iva: "+prodotto1.getPrice());
+    }
 }
+
--- src/org/oop2/Prodotto.java
+++ src/org/oop2/Prodotto.java
@@ -1,2 +1,78 @@
-package org.oop2;public class Prodotto {
+package org.oop2;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Random;
+
+public class Prodotto {
+
+
+    private String name;
+    private String description;
+    private BigDecimal  price ;
+    private BigDecimal iva ;
+    private String code;
+    private BigDecimal  pricewithiva ;
+
+    /* Costruttore*/
+    public Prodotto(String name, String description, String price, String iva){
+        this.name=name;
+        this.description=description;
+        this.price=new BigDecimal(price);
+        this.iva= new BigDecimal(iva);
+        /*formatto una stringa a 6 numeri*/
+        this.code=String.format("%06d", generateCode());;
+        this.pricewithiva=generatetPriceWithIva();
+    }
+
+    /*metodo che genera il codice */
+    private int generateCode(){
+        Random random = new Random();
+        int barcode=random.nextInt(1000000);
+        return barcode;
+    }
+
+    /*metodo che imposta il prezzo con l'aggiuunta dell'iva*/
+    private BigDecimal generatetPriceWithIva(){
+        BigDecimal oneundred = new BigDecimal("100");
+        BigDecimal added =(this.price.multiply(this.iva)).divide(oneundred, 2 , RoundingMode.HALF_UP);
+        BigDecimal result=this.price.add(added);
+        return result;
+    }
+
+
+    /*getter*/
+    public String getCode(){
+        return this.code;
+    }
+
+    public  String getNameComplete(){
+        return (this.code+"-"+this.name);
+    }
+
+    public String getPriceWithIva(){
+    return this.pricewithiva.toString();
+    }
+
+    public String getPrice(){
+        return this.price.toString();
+    }
+
+/*setter*/
+    public void setName(String name){
+        this.name=name;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
